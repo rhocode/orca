46e3dda4da13bafba7dd2051d47cc181738e5289
Adam Chao <adam@thinkaliker.com>
2015-11-07 12:03:35 -0800

    service layer test

--- app/src/main/java/com/rhocode/orca/MainActivity.java
+++ app/src/main/java/com/rhocode/orca/MainActivity.java
@@ -5,18 +5,86 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.esri.android.map.GraphicsLayer;
+import com.esri.android.map.MapOptions;
+import com.esri.android.map.MapView;
+import com.esri.android.map.ags.ArcGISFeatureLayer;
+import com.esri.android.map.event.OnStatusChangedListener;
+import com.esri.core.geometry.Polygon;
+
 public class MainActivity extends ActionBarActivity {
+
+    // The MapView.
+    private MapView mMapView = null;
+
+    // The basemap switching menu items.
+    private MenuItem mStreetsMenuItem = null;
+    private MenuItem mTopoMenuItem = null;
+    private MenuItem mGrayMenuItem = null;
+    private MenuItem mOceansMenuItem = null;
+
+    ArcGISFeatureLayer mFeatureLayer;
+    GraphicsLayer mGraphicsLayer;
+    boolean mIsMapLoaded;
+    String mFeatureServiceURL;
+
+    // Create MapOptions for each type of basemap.
+    private final MapOptions mTopoBasemap = new MapOptions(MapOptions.MapType.TOPO);
+    private final MapOptions mStreetsBasemap = new MapOptions(MapOptions.MapType.STREETS);
+    private final MapOptions mGrayBasemap = new MapOptions(MapOptions.MapType.GRAY);
+    private final MapOptions mOceansBasemap = new MapOptions(MapOptions.MapType.OCEANS);
+
+    // The current map extent, use to set the extent of the map after switching basemaps.
+    private Polygon mCurrentMapExtent = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Retrieve the map and initial extent from XML layout
+        mMapView = (MapView) findViewById(R.id.map);
+
+        // Get the feature service URL from values->strings.xml
+        mFeatureServiceURL = this.getResources().getString(R.string.featureServiceURL);
+        // Add Feature layer to the MapView
+        mFeatureLayer = new ArcGISFeatureLayer(mFeatureServiceURL, ArcGISFeatureLayer.MODE.ONDEMAND);
+        mMapView.addLayer(mFeatureLayer);
+
+        // Set the Esri logo to be visible, and enable map to wrap around date line.
+        mMapView.setEsriLogoVisible(true);
+        mMapView.enableWrapAround(true);
+
+        // Set a listener for map status changes; this will be called when switching basemaps.
+        mMapView.setOnStatusChangedListener(new OnStatusChangedListener() {
+
+            private static final long serialVersionUID = 1L;
+
+            public void onStatusChanged(Object source, STATUS status) {
+                // Set the map extent once the map has been initialized, and the basemap is added
+                // or changed; this will be indicated by the layer initialization of the basemap layer. As there is only
+                // a single layer, there is no need to check the source object.
+                if (STATUS.LAYER_LOADED == status) {
+                    mMapView.setExtent(mCurrentMapExtent);
+                }
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Get the basemap switching menu items.
+        mStreetsMenuItem = menu.getItem(0);
+        mTopoMenuItem = menu.getItem(1);
+        mGrayMenuItem = menu.getItem(2);
+        mOceansMenuItem = menu.getItem(3);
+
+        // Also set the topo basemap menu item to be checked, as this is the default.
+        mTopoMenuItem.setChecked(true);
+
         return true;
     }
 
@@ -27,11 +95,41 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
+        // Save the current extent of the map before changing the map.
+        mCurrentMapExtent = mMapView.getExtent();
+
+        // Handle menu item selection.
+        switch (item.getItemId()) {
+            case R.id.World_Street_Map:
+                mMapView.setMapOptions(mStreetsBasemap);
+                mStreetsMenuItem.setChecked(true);
+                return true;
+            case R.id.World_Topo:
+                mMapView.setMapOptions(mTopoBasemap);
+                mTopoMenuItem.setChecked(true);
+                return true;
+            case R.id.Gray:
+                mMapView.setMapOptions(mGrayBasemap);
+                mGrayMenuItem.setChecked(true);
+                return true;
+            case R.id.Ocean_Basemap:
+                mMapView.setMapOptions(mOceansBasemap);
+                mOceansMenuItem.setChecked(true);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    protected void onPause() {
+        super.onPause();
+        mMapView.pause();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mMapView.unpause();
     }
 }
